c00dc637ca43921cd0ad267447eaad2949f7d4f6
GitHub Action <action@github.com>
2023-01-29 04:08:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/solarisneko/ijplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/solarisneko/ijplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.solarisneko.ijplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.solarisneko.ijplugintemplate.MyBundle
 
 class MyApplicationService {
 
